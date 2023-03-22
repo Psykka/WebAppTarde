b2b0cc5794e576bfcb4f918586f2c5ec371e3ae7
Psykka <36780964+Psykka@users.noreply.github.com>
2023-03-22 16:19:19 -0300

    Add project files.

---
+++ WebAppTarde/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTarde.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
