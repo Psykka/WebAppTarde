811404d1f043440bd14c9be808e45c1ea159f4b3
Psykka <36780964+Psykka@users.noreply.github.com>
2023-04-19 16:04:05 -0300

    feat(Client): all controller and views

--- WebAppTarde/Controllers/ClientController.cs
+++ WebAppTarde/Controllers/ClientController.cs
@@ -1,17 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppTarde.Models;
 
 namespace WebAppTarde.Controllers
 {
     public class ClientController : Controller
     {
+        public static List<ClientViewModel> db = new List<ClientViewModel>();
+
         public IActionResult List()
         {
-            return View();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ClientViewModel client = new ClientViewModel();
+            //    client.Id = i;
+            //    client.Nome = "Pessoa " + i;
+            //    client.Telefone = "16 " + i;
+
+            //    db.Add(client);
+            //}
+            return View(db);
         }
 
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(ClientViewModel data)
+        {
+            if(data.Id == 0)
+            {
+                int id = db.Count + 1;
+                data.Id = id;
+                db.Add(data);
+            }
+            else
+            {
+                int index = db.FindIndex(x => x.Id == data.Id);
+                db[index] = data;
+            }
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            ClientViewModel client = db.Find(x => x.Id == id);
+            if(client != null) db.Remove(client);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ClientViewModel client = db.Find(x => x.Id == id);
+
+            if (client != null)
+            {
+                return View(client);
+            }
+            else
+            {
+                return RedirectToAction("List");
+            }
         }
     }
 }
---
+++ WebAppTarde/Models/ClientViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppTarde.Models
+{
+    public class ClientViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
